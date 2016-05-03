fb81c4161a6e14d52dac5e71454979df6a6ce7b2
Florent VIOLIN <fviolin@student.42.fr>
2016-05-03 12:31:18 +0200

    testing termcaps functions for initialization

--- include/ft_select.h
+++ include/ft_select.h
@@ -6,9 +6,21 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 10:00:16 by fviolin           #+#    #+#             */
-/*   Updated: 2016/05/03 10:00:39 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/05/03 12:00:32 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_SELECT
+# define FT_SELECT
+
+#include "../libft/libft.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h> // printf for tests
+#include <term.h>
+#include <termios.h>
+#include <termcap.h>
+#include <curses.h>
+#include <signal.h>
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,44 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 10:05:35 by fviolin           #+#    #+#             */
-/*   Updated: 2016/05/03 10:06:09 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/05/03 12:29:40 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_select.h"
 
-int		main(int ac, char **av)
+int		init_terminal_data()
 {
+	char	*termtype;
+	char	termbuffer[2048];
+	int		result;
+
+	ft_bzero(termbuffer, 2048);
+	result = 0;
+	if (!(termtype = getenv("TERM")))
+		return (-1);
+	result = tgetent(termbuffer, termtype);
+	if (result < 0)
+		ft_putendl_fd("Could not access the termcap data base.\n", 2);
+	if (result == 0)
+	{
+		ft_putendl_fd("Terminal type is ", 2);
+		ft_putstr_fd(termtype, 2);
+		ft_putendl_fd(" is not defined.\n", 2);
+	}
+	return (0);
+}
+
+int		main(int ac, char **av, char **env)
+{
+	av = NULL;
+	env = NULL;
+	if (ac < 2)
+	{
+		ft_putendl_fd("Error: wrong usage", 2);
+		exit (1);
+	}
+	if (!init_terminal_data())
+		printf("test\n");
 	return (0);
 }
